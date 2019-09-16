479b1a8da5a86ef1b4505aa8ed6019ce66f25348
Joao <ritterjv@gmail.com>
2019-09-15 18:14:55 -0700

    -

--- app.js
+++ app.js
@@ -8,19 +8,29 @@ const expressMiddleware = require("@sustainers/express-middleware");
 
 expressMiddleware(app);
 
-app.post(
+app.use((req, _, next) => {
+  logger.info("Request: ", {
+    params: req.params,
+    body: req.body,
+    query: req.query,
+    headers: req.headers
+  });
+  next();
+});
+
+app.get(
   "/",
   asyncHandler(async (req, res) => {
-    logger.info("Request: ", {
-      params: req.params,
-      body: req.body,
-      query: req.query,
-      headers: req.headers
-    });
-    res.send("🍉");
+    res.send([
+      {
+        title: "Roof",
+        requestTitle: req.query.title,
+        people: ["Joao", "Tyler"],
+        sideButtons: [{ title: "Do this" }, { title: "Do that" }]
+      }
+    ]);
   })
 );
 
 app.use(errorMiddleware);
-
 module.exports = app;
